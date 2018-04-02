fb46b7ee166e84adf7060da61385ceb5191bb417
smokris <smokris@softpixel.com>
2018-04-02 10:58:01 -0400

    Allow linking to system libraries.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -19,6 +19,6 @@ class MuParserTestConan(ConanFile):
             self.run('! (otool -L lib/libmuparser.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
             self.run('! (otool -l lib/libmuparser.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/libmuparser.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libmuparser.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
