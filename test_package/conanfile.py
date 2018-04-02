c848e546407c82778b1a5502a5dffa5c08e54b59
smokris <smokris@softpixel.com>
2018-04-02 10:34:34 -0400

    Test LC_RPATH.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,15 +9,16 @@ class MuParserTestConan(ConanFile):
 
     def imports(self):
         self.copy('*', src='bin', dst='bin')
-        self.copy('*', dst='lib', src='lib')
+        self.copy('*', src='lib', dst='lib')
 
     def test(self):
         self.run('qbs run')
 
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
-            self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libmuparser.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -l lib/libmuparser.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libmuparser.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
