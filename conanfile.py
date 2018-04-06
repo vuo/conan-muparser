bc4a8136f229143392c2f6ebf7e692c6c45191ef
Steve Mokris <smokris@softpixel.com>
2018-04-06 16:30:42 -0400

    Increment package version.

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class MuParserConan(ConanFile):
     name = 'muparser'
 
     source_version = '2.2.5'
-    package_version = '2'
+    package_version = '3'
     version = '%s-%s' % (source_version, package_version)
 
     build_requires = 'llvm/3.3-5@vuo/stable', \
